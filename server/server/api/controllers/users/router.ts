3516841bc598f27039552cc2ada1c34d1765c502
Ahmad Izzuddin <niduzi@protonmail.com>
2021-05-17 01:34:25 +0700

    completed endpoints for user base paths

--- server/server/api/controllers/users/router.ts
+++ server/server/api/controllers/users/router.ts
@@ -1,21 +1,125 @@
 import express from 'express';
 import controller from './controller';
 import checkPermissions from '../../middlewares/permissions.handler';
+import { Roles } from '../../interfaces/roles.enum';
+import multer from 'multer';
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 export default express
   .Router()
-  .post('/', controller.register)
-  .get('/', checkPermissions([{ role: 'admin' }]), controller.all)
+  .get('/', checkPermissions([{ role: Roles.Admin }]), controller.getAll)
+  .post('/', controller.postRegister)
   .get(
     '/:id',
     checkPermissions([
       {
-        role: 'user',
+        role: Roles.User,
         idValidator: (id: number, userId: number): boolean => {
           return id == userId;
         },
       },
+      {
+        role: Roles.Admin,
+      },
     ]),
-    controller.byId
+    controller.getById
   )
-  .post('/authenticate', controller.login);
+  .put(
+    '/:id',
+    checkPermissions([
+      {
+        role: Roles.User,
+        idValidator: (id: number, userId: number): boolean => {
+          return id == userId;
+        },
+      },
+      {
+        role: Roles.Admin,
+      },
+    ]),
+    upload.single('profilePicture'),
+    controller.putUpdateUser
+  )
+  .get(
+    '/:id/profilePicture',
+    checkPermissions([
+      {
+        role: Roles.User,
+        idValidator: (id: number, userId: number): boolean => {
+          return id == userId;
+        },
+      },
+      {
+        role: Roles.Admin,
+      },
+    ]),
+    controller.getProfilePicture
+  )
+  .get(
+    '/:id/notifications',
+    checkPermissions([
+      {
+        role: Roles.User,
+        idValidator: (id: number, userId: number): boolean => {
+          return id == userId;
+        },
+      },
+      {
+        role: Roles.Admin,
+      },
+    ]),
+    controller.getNotifications
+  )
+  .get(
+    '/:id/refresh-tokens',
+    checkPermissions([{ role: Roles.Admin }]),
+    controller.getUserRefreshTokens
+  )
+  .get(
+    '/:id/reservations',
+    checkPermissions([
+      {
+        role: Roles.User,
+        idValidator: (id: number, userId: number): boolean => {
+          return id == userId;
+        },
+      },
+      {
+        role: Roles.Admin,
+      },
+    ]),
+    controller.getReservations
+  )
+  .put(
+    '/terminate/:id',
+    checkPermissions([{ role: Roles.Admin }]),
+    controller.putTerminate
+  )
+  .delete(
+    '/terminate/:id',
+    checkPermissions([{ role: Roles.Admin }]),
+    controller.deleteTerminate
+  )
+  .put(
+    '/suspend/:id',
+    checkPermissions([{ role: Roles.Admin }]),
+    controller.putSuspension
+  )
+  .delete(
+    '/suspend/:id',
+    checkPermissions([{ role: Roles.Admin }]),
+    controller.deleteSuspension
+  )
+  .post(
+    '/warn/:id',
+    checkPermissions([{ role: Roles.Admin }]),
+    controller.postWarn
+  )
+  .post('/authenticate', controller.postLogin)
+  .post('/refresh-token', controller.postRefreshToken)
+  .post(
+    '/revoke-token',
+    checkPermissions([{ role: Roles.Admin }]),
+    controller.postRevokeToken
+  );
